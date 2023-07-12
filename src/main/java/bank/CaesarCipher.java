01203aaacebef347b8c0c9cdff20fe7cf45acae5
Bobby St. Jacques <rjsvcs@rit.edu>
2023-07-12 14:42:12 -0400

    added javadocs and reversed encryption to the main

--- src/main/java/bank/CaesarCipher.java
+++ src/main/java/bank/CaesarCipher.java
@@ -6,14 +6,35 @@ import java.util.Scanner;
  * A simple implementation of a Caesar Cipher.
  */
 public class CaesarCipher {
+    /**
+     * Encrypts a single character and returns it.
+     * 
+     * @param c The character to encrypt.
+     * @param shift The amount by which the character should be shifted.
+     * @return The encrypted character.
+     */
     public static char encrypt(char c, int shift) {
         return (char)(c + shift);
     }
 
+    /**
+     * Decrypts a single character and returns it.
+     * 
+     * @param c The character to decrypt.
+     * @param shift The amount by which the plaintext character was encrypted.
+     * @return The decrypted character.
+     */
     public static char decrypt(char c, int shift) {
         return encrypt(c, -shift);
     }
 
+    /**
+     * Enncrypts an entire string of plaintext and returns the ciphertext.
+     * 
+     * @param plaintext The plaintext to encrypt.
+     * @param shift The amount by which the characters should be shifted.
+     * @return The ciphertext.
+     */
     public static String encrypt(String plaintext, int shift) {
         StringBuilder builder = new StringBuilder();
         for(char c : plaintext.toCharArray()) {
@@ -23,6 +44,13 @@ public class CaesarCipher {
         return builder.toString();
     }
 
+    /**
+     * Decrypts an entire string of ciphertext and returns the plaintext.
+     * 
+     * @param ciphertext The ciphertext to decrypt.
+     * @param shift The amount by which the plaintext was originally shifted.
+     * @return The plaintext.
+     */
     public static String decrypt(String ciphertext, int shift) {
         return encrypt(ciphertext, -shift);
     }
@@ -42,6 +70,8 @@ public class CaesarCipher {
                 } else {
                     String ciphertext = encrypt(plaintext, shift);
                     System.out.println("ciphertext: " + ciphertext);
+                    String reversed = decrypt(ciphertext, shift);
+                    System.out.println("reversed: " + reversed);
                 }
             }
         } catch(Exception e) {
