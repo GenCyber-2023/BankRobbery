461dddab96cebe2f19f63f9d5a7dcf79398f1ea0
Bobby St. Jacques <rjsvcs@rit.edu>
2023-07-12 15:16:48 -0400

    still sorting logging out

--- src/main/java/server/BankClientHandler.java
+++ src/main/java/server/BankClientHandler.java
@@ -2,6 +2,8 @@ package server;
 
 import java.io.IOException;
 import java.net.Socket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import bank.BankCo;
 
@@ -9,6 +11,8 @@ import static bank.CaesarCipher.decrypt;
 import static bank.CaesarCipher.encrypt;
 
 public class BankClientHandler extends HandlerThread implements BankCo {
+    private static final Logger LOGGER = 
+        Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
 
     protected BankClientHandler(Socket socket) throws IOException {
         super(socket);
@@ -17,9 +21,19 @@ public class BankClientHandler extends HandlerThread implements BankCo {
     @Override
     public String handleMessage(String message) {
         String plaintext = decrypt(message, BANK_SHIFT);
+        log("REQUEST: " + plaintext);
         String response = plaintext.toUpperCase();
+        log("RESPONSE: " + response);
         String ciphertext = encrypt(response, BANK_SHIFT);
         return ciphertext;
+    }
+
+    private void log(String message) {
+        log(Level.INFO, message);
+    }
+
+    private void log(Level level, String message) {
+        LOGGER.log(level, toString() + ": " + message);
     }
     
 }
--- src/main/java/server/HandlerThread.java
+++ src/main/java/server/HandlerThread.java
@@ -2,15 +2,10 @@ package server;
 
 import java.io.IOException;
 import java.net.Socket;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 import bank.Duplexer;
 
 public abstract class HandlerThread extends Duplexer implements Runnable {
-    protected static final Logger LOGGER = 
-        Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
-
     protected HandlerThread(Socket socket) throws IOException {
         super(socket);
     }
@@ -24,8 +19,6 @@ public abstract class HandlerThread extends Duplexer implements Runnable {
                 String response = handleMessage(request);
                 send(response);
             } catch (IOException ioe) {
-                LOGGER.log(Level.SEVERE, "Error communicatinig with client: "
-                    + ioe.getMessage());
                 sentinel = false;
             }
         }
