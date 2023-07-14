8308594ee36988862d8cb189b26368aa55dac899
Bobby St. Jacques <rjsvcs@rit.edu>
2023-07-14 10:19:30 -0400

    adding more javadoc

--- src/main/java/hacks/Messenger.java
+++ src/main/java/hacks/Messenger.java
@@ -2,13 +2,41 @@ package hacks;
 
 import java.net.InetAddress;
 
+/**
+ * A simple implementation of a messenger that can be observer by a single
+ * {@link MessageObserver}.
+ */
 public class Messenger {
+    /**
+     * The register observer.
+     */
     private MessageObserver observer;
 
+    /**
+     * Default constructor sets the observer to null.
+     */
+    protected Messenger() {
+        this.observer = null;
+    }
+
+    /**
+     * Registers an interested {@link MessageObserver} to be notified when
+     * the messenger is used to send or receive a message.
+     * 
+     * @param observer The obserger to register.
+     */
     public void setOnMessage(MessageObserver observer) {
         this.observer = observer;
     }
 
+    /**
+     * Used to notify the registered observer that the messenger has been used 
+     * to send a message.
+     * 
+     * @param recipient The address of the host to which the message was sent.
+     * @param port The port to which the message was sent.
+     * @param message The message.
+     */
     protected void messageSent(InetAddress recipient, int port, 
             String message) {
         if(this.observer != null) {
@@ -16,6 +44,14 @@ public class Messenger {
         }
     }
 
+    /**
+     * Used to notify the registered observer that the messenger has been used 
+     * to receive a message.
+     * 
+     * @param sender The address of the host to which the message was sent.
+     * @param port The port to which the message was sent.
+     * @param message The message.
+     */
     protected void messageReceived(InetAddress sender, int port, 
         String message) {
         if(this.observer != null) {
