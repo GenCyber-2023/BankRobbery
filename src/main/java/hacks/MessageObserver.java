8046b0cf1b08bc8e5419979cd671d602b4377873
Bobby St. Jacques <rjsvcs@rit.edu>
2023-07-14 10:16:20 -0400

    adding more javadoc

--- src/main/java/hacks/MessageObserver.java
+++ src/main/java/hacks/MessageObserver.java
@@ -8,18 +8,22 @@ import java.net.InetAddress;
  */
 public interface MessageObserver {
     /**
+     * This method will be called on a registered observer whenever the 
+     * messenger that it is observing receives a message.
      * 
-     * @param sender
-     * @param port
-     * @param message
+     * @param sender The address of the host that sent the message.
+     * @param port The port from which the message was sent.
+     * @param message The content of the message.
      */
     public void messageReceived(InetAddress sender, int port, String message);
 
     /**
+     * This method will be called on a registered observer whenever the 
+     * messenger that it is observing sends a message.
      * 
-     * @param recipient
-     * @param port
-     * @param message
+     * @param recipient The address of the host receiving the message.
+     * @param port The port to which the message is being sent.
+     * @param message The content of the message.
      */
     public void messageSent(InetAddress recipient, int port, String message);
 }
